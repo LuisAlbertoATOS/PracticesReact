b5365b4d1e24367819e295603cc5e5ce5a489731
Luis Perez <luis-alberto.perez@atos.net>
2022-08-31 16:17:00 -0500

    finished my first app

--- my-first-react-app/my-first-app/src/components/Expenses/ExpenseItem.js
+++ my-first-react-app/my-first-app/src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
-    <section className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <p className="expense-item__price">${props.amount}</p>
       </div>
-    </section>
+    </Card>
   );
 }
 
