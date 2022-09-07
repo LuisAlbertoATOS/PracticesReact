0ddac284e6f312c73816ddad16c81c52448181a9
Luis Perez <luis-alberto.perez@atos.net>
2022-09-07 12:11:39 -0500

    post request with ref

--- my-ts-app/my-typescript-app/src/App.tsx
+++ my-ts-app/my-typescript-app/src/App.tsx
@@ -3,44 +3,62 @@ import React, { useState, useEffect, useCallback } from "react";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 import { IMovie } from "./interfaces/Movie.interface";
-import { IMovieData } from "./interfaces/MovieData.interface";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState<IMovie[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [error, setError] = useState<Error|null>(null);
+  const [error, setError] = useState<Error | null>(null);
 
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
-    
-    try {
-      const response = await fetch("https://swapi.dev/api/films/");
-      const data = await response.json();
 
+    try {
+      // const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-star-wars-787af-default-rtdb.firebaseio.com/movies.json"
+      );
+      
       if (!response.ok) {
         throw new Error("Something went wrong!");
       }
       
-      const transformedMovies = data.results.map((movieData:IMovieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
-    } catch (error:any) {
+      const data = await response.json();
+      const loadedMovies = [];
+
+      for (const key in data){
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      } 
+      setMovies(loadedMovies);
+    } catch (error: any) {
       setError(error);
     }
     setIsLoading(false);
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
-  
+
+  async function addMovieHandler(movie:any) {
+
+    const response = await fetch(
+      "https://react-http-star-wars-787af-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    const data = await response.json();
+  }
+
   let content = <p>Found no movies</p>;
   if (movies.length > 0) {
     content = <MoviesList movies={movies}></MoviesList>;
@@ -54,6 +72,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler}></AddMovie>
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
--- my-ts-app/my-typescript-app/src/interfaces/Movie.interface.ts
+++ my-ts-app/my-typescript-app/src/interfaces/Movie.interface.ts
@@ -1,5 +1,5 @@
 export interface IMovie {
-  id:string,
+  id?:string,
   title:string,
   releaseDate:string,
   openingText:string
