a1fd586d31963188304fc3d804d76c7ed7454b1b
Luis Perez <luis-alberto.perez@atos.net>
2022-09-12 15:03:04 -0500

    redux practice

---
+++ redux/redux-app-2/redux-app-2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './features/counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello</h2>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
